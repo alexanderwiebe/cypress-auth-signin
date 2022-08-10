c9df47e9c7f4b7da493c82a208d4a7bc8dae65d1
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-08-10 08:19:29 -0600

    Update sign in to work locally

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css'],
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"],
 })
 export class SignInComponent implements OnInit {
   form = this.fb.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required],
+    username: ["", Validators.required],
+    password: ["", Validators.required],
   });
 
   constructor(private fb: FormBuilder, private router: ActivatedRoute) {}
@@ -18,11 +18,12 @@ export class SignInComponent implements OnInit {
   ngOnInit() {}
 
   submit() {
-    if (this.form.get('password').value === 'password') {
-      console.log(this.router.snapshot.queryParamMap.get('url'));
-      window.location.assign(
-        `${this.router.snapshot.queryParamMap.get('url')}&token=asdf1234`
-      );
+    if (this.form.get("password").value === "password") {
+      const newURI = `http${this.router.snapshot.queryParamMap
+        .get("url")
+        .slice(5)}?token=asdf1234`;
+      console.log(newURI);
+      window.location.assign(newURI);
     }
   }
 }
